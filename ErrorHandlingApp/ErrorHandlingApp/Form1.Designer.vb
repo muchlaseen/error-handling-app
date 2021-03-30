7ba623125242e462d4c0fc0bed7c6cba44d78bf8
Achmad Muchlasin <muchlaseen@gmail.com>
2021-03-30 07:41:37 +0700

    First Commit

---
+++ ErrorHandlingApp/ErrorHandlingApp/Form1.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtIndex = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtList = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtError = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(41, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(36, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Index:"
+        '
+        'txtIndex
+        '
+        Me.txtIndex.Location = New System.Drawing.Point(44, 36)
+        Me.txtIndex.Name = "txtIndex"
+        Me.txtIndex.Size = New System.Drawing.Size(100, 20)
+        Me.txtIndex.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(44, 62)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(221, 34)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Tampilkan"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtList
+        '
+        Me.txtList.Location = New System.Drawing.Point(44, 153)
+        Me.txtList.Name = "txtList"
+        Me.txtList.Size = New System.Drawing.Size(221, 20)
+        Me.txtList.TabIndex = 3
+        Me.txtList.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(41, 137)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(75, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Data pada list:"
+        '
+        'txtError
+        '
+        Me.txtError.Location = New System.Drawing.Point(44, 211)
+        Me.txtError.Multiline = True
+        Me.txtError.Name = "txtError"
+        Me.txtError.Size = New System.Drawing.Size(221, 120)
+        Me.txtError.TabIndex = 5
+        Me.txtError.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(41, 195)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Pesan Error:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(70, 391)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(172, 24)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Achmad Muchlasin"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(93, 415)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(129, 20)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "06.2018.1.06941"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(97, 337)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(115, 30)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Reset"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(314, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtError)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtList)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtIndex)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "ErrorHandling | 06.2018.1.06941"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtIndex As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtList As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtError As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button2 As Button
+End Class
---
+++ ErrorHandlingApp/ErrorHandlingApp/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+
+    Dim ListKaryawan As New List(Of String)
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListKaryawan.Add("Achmad Muchlasin")
+        ListKaryawan.Add("Reza Kurniawan")
+        ListKaryawan.Add("Agus Susanto")
+        ListKaryawan.Add("Andri Setiawan")
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Try
+            txtList.Text = ListKaryawan(CInt(txtIndex.Text))
+        Catch ex As Exception
+            txtError.Text = ex.Message
+        Finally
+            MessageBox.Show("Nilai index dicari: " + txtIndex.Text)
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        txtIndex.Text = ""
+        txtList.Text = ""
+        txtError.Text = ""
+    End Sub
+End Class
